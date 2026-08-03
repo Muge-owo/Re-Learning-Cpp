bfc7c2bafaf136dec859c18bc9c2bc9b2c48fb3a
Muge-owo <3516169371@qq.com>
2026-08-03 20:37:31 +0800

    请输入文本...字母金字塔沟史逻辑plus

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-// #include <algorithm>
+#include <iomanip>
 
 int main() {
-    std::string s{};
+    char ch{};
+    std::cin >> ch;
 
-    std::getline(std::cin, s);
-
-    // 法1 : 包含 algorithm 头文件，使用 std::reverse(首, 尾) 来反转
-    // std::reverse(s.begin(), s.end());
-    // std::cout << s << std::endl;
-
-    // 法2 : 循环的方式 遍历输出每个字节
-    // for(int i = s.size() - 1; i >= 0; i--)
-    for(int i = s.length() - 1; i >= 0; i--)
-        std::cout << s[i];
-    std::cout << std::endl;
+    if(ch >= 'A' && ch <= 'Z')
+        for(int i = ch - 'A'; i >= 0; i--){
+            std::cout << std::setw(i+1) << std::setfill(' ') << 'A';
+            for(int j = 1; j <= ch-'A'-i; j++)
+                std::cout << static_cast<char>(j+'A');
+            for(int j = ch-'A'-i-1; j >= 0; j--)
+                std::cout << static_cast<char>(j+'A');
+            std::cout << std::endl;
+        }
 
     return 0;
 }
