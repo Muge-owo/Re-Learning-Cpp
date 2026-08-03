03a8bca98a1cf7c93e1eca8164e8e6a1163985d3
Muge-owo <3516169371@qq.com>
2026-08-03 18:50:33 +0800

    完成02-variables-and-types练习

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, C++!" << std::endl;
+    int a{0}, b{0};
+
+    std::cin >> a >> b;
+    std::cout << a+b << std::endl;
 
     return 0;
 }
