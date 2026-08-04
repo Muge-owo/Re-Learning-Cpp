e288a8f7ce256b4be26c693dd2dd87337e511e22
Muge-owo <3516169371@qq.com>
2026-08-04 14:02:08 +0800

    修复当输入不匹配时输出为空(无换行)的错误...居然是把换行和文本分开的吗...

--- main.cpp
+++ main.cpp
@@ -12,12 +12,21 @@ int main() {
     int a{};
     std::cin >> a;
     
-    if(a % 15 == 0)
-        std::cout << "FizzBuzz" << std::endl;
-    else if(a % 3 == 0)
-        std::cout << "Fizz" << std::endl;
-    else if(a % 5 == 0)
-        std::cout << "Buzz" << std::endl;
+    // if(a % 15 == 0)
+    //     std::cout << "FizzBuzz";
+    // else if(a % 3 == 0)
+    //     std::cout << "Fizz";
+    // else if(a % 5 == 0)
+    //     std::cout << "Buzz";
+    // std::cout << std::endl;
+
+    
+    if(a % 3 == 0)
+        std::cout << "Fizz";
+    if(a % 5 == 0)
+        std::cout << "Buzz";
+    std::cout << std::endl;
+
 
     // 试图通过读取输出缓冲区是否存在东西, 来判断是否需要在末尾换行
     // 失敗した失敗した失敗した失敗した失敗した
