9a1e47ee479a6b25f8523fb442877d04770caf17
Muge-owo <3516169371@qq.com>
2026-08-04 13:57:10 +0800

    完成06-conditionals练习

--- main.cpp
+++ main.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
-#include <iomanip>
+// #include <streambuf>
+
+// class BufferInspector : public std::streambuf {
+// public:
+//     char *getPBase() const { return pbase(); }
+//     char *getPPtr() const { return pptr(); }
+//     char *getEPPtr() const { return epptr(); }
+// };
 
 int main() {
-    std::string s1{};
+    int a{};
+    std::cin >> a;
+    
+    if(a % 15 == 0)
+        std::cout << "FizzBuzz" << std::endl;
+    else if(a % 3 == 0)
+        std::cout << "Fizz" << std::endl;
+    else if(a % 5 == 0)
+        std::cout << "Buzz" << std::endl;
 
-    std::getline(std::cin, s1);
+    // 试图通过读取输出缓冲区是否存在东西, 来判断是否需要在末尾换行
+    // 失敗した失敗した失敗した失敗した失敗した
+    // if(a % 3 == 0)
+    //     std::cout << "Fizz";
+    // if(a % 5 == 0)
+    //     std::cout << "Buzz";
+    
+    // BufferInspector bufcout;
+    // std::cout.rdbuf(&bufcout);
 
-    // 寻找子串并返回从其开始的字符串
-    if(s1.find("lo") != std::string::npos)
-        std::cout << s1.substr(s1.find("lo")) << std::endl;
-    else
-        std::cout << "None" << std::endl;
+    // if(bufcout.getPBase() != bufcout.getEPPtr())
+    //     std::cout << "+++" << std::endl;
 
     return 0;
 }
