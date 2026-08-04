8c02ae2564c27a668deac3b3f18e89a920813175
Muge-owo <3516169371@qq.com>
2026-08-04 20:33:47 +0800

    完成07-loops练习

--- main.cpp
+++ main.cpp
@@ -1,47 +1,15 @@
 #include <iostream>
-// #include <streambuf>
 
-// class BufferInspector : public std::streambuf {
-// public:
-//     char *getPBase() const { return pbase(); }
-//     char *getPPtr() const { return pptr(); }
-//     char *getEPPtr() const { return epptr(); }
-// };
+int main(int argc, const char *argv[])
+{
+    int n{};
+    std::cin >> n;
 
-int main() {
-    int a{};
-    std::cin >> a;
-    
-    // if(a % 15 == 0)
-    //     std::cout << "FizzBuzz";
-    // else if(a % 3 == 0)
-    //     std::cout << "Fizz";
-    // else if(a % 5 == 0)
-    //     std::cout << "Buzz";
-    // std::cout << std::endl;
+    long long total{};
+    for(int i = n; i > 0; i--)
+        total += i;
 
-    
-    if(a % 3 == 0)
-        std::cout << "Fizz";
-    if(a % 5 == 0)
-        std::cout << "Buzz";
-    if(a % 5 && a % 3)
-        std::cout << a;
-    std::cout << std::endl;
-
-
-    // 试图通过读取输出缓冲区是否存在东西, 来判断是否需要在末尾换行
-    // 失敗した失敗した失敗した失敗した失敗した
-    // if(a % 3 == 0)
-    //     std::cout << "Fizz";
-    // if(a % 5 == 0)
-    //     std::cout << "Buzz";
-    
-    // BufferInspector bufcout;
-    // std::cout.rdbuf(&bufcout);
-
-    // if(bufcout.getPBase() != bufcout.getEPPtr())
-    //     std::cout << "+++" << std::endl;
+    std::cout << total << std::endl;
 
     return 0;
 }
