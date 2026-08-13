378790c2781baf501b03e3dececd4a67cfd0476d
Muge-owo <3516169371@qq.com>
2026-08-13 19:02:54 +0800

    完成13-classes练习

--- main.cpp
+++ main.cpp
@@ -1,42 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric>
+
+class Rectangle{
+private:
+    int width;
+    int height;
+
+public:
+    Rectangle(int w, int h) :width(w), height(h) {}
+
+    int area() const { return width * height; }
+    int perimeter() const { return 2 * (width + height); }
+};
 
 int main(int argc, const char *argv[])
 {
-    std::vector<int> vInt{};
+    int w{}, h{};
+    std::cin >> w >> h;
 
-    long long tmp;
-    while(std::cin >> tmp){
-        vInt.push_back(tmp);
-    }
+    Rectangle rec1(w, h);
 
-    long long total = std::accumulate(vInt.begin(), vInt.end(), 0LL \
-    // );       // 不加参数直接累加
-    , [](const long long all, int a){ return a % 2 == 0 ? all + (a * a) : all; });  // 添加 lambda 函数指定规则
-
-    std::cout << total << std::endl;
+    std::cout << "area: " << rec1.area() << std::endl;
+    std::cout << "perimeter: " << rec1.perimeter() << std::endl;
 
     return 0;
 }
-
-// int main(int argc, const char *argv[])
-// {
-//     std::vector<int> vInt{};
-
-//     int tmp;
-//     while(std::cin >> tmp){
-//         if(tmp % 2 == 0)
-//             vInt.push_back(tmp);
-//     }
-
-//     long long total{};
-//     for(int &a: vInt){
-//         total += (a*a);
-//     }
-
-//     std::cout << total << std::endl;
-
-//     return 0;
-// }
