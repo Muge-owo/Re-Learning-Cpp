0d99626e23b0bf244a016fb60a0d88865c4e0c78
Muge-owo <3516169371@qq.com>
2026-08-13 18:40:56 +0800

    完成12-algorithm练习

--- main.cpp
+++ main.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
-#include <map>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 
 int main(int argc, const char *argv[])
 {
-    std::map<std::string, int>  mWord{};
+    std::vector<int> vInt{};
 
-    std::string word;
-    while(std::cin >> word){
-        mWord[word]++;
+    long long tmp;
+    while(std::cin >> tmp){
+        vInt.push_back(tmp);
     }
 
-    std::cout << mWord.size() << std::endl;
+    long long total = std::accumulate(vInt.begin(), vInt.end(), 0LL \
+    // );       // 不加参数直接累加
+    , [](const long long all, int a){ return a % 2 == 0 ? all + (a * a) : all; });  // 添加 lambda 函数指定规则
+
+    std::cout << total << std::endl;
 
     return 0;
 }
+
+// int main(int argc, const char *argv[])
+// {
+//     std::vector<int> vInt{};
+
+//     int tmp;
+//     while(std::cin >> tmp){
+//         if(tmp % 2 == 0)
+//             vInt.push_back(tmp);
+//     }
+
+//     long long total{};
+//     for(int &a: vInt){
+//         total += (a*a);
+//     }
+
+//     std::cout << total << std::endl;
+
+//     return 0;
+// }
