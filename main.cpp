403ae4d83dde67093c3ba1e1d5a2a7468776303f
Muge-owo <3516169371@qq.com>
2026-08-14 16:01:48 +0800

    おめでとう👏おめでとう👏

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
-#include "main.h"
+#include <iostream>
+#include <vector>
 
 int main(int argc, const char *argv[])
 {
-    int a{}, b{};
-    double x{}, y{};
+    std::vector<std::string> oWari{};
+    oWari.push_back("完全勝利だぜ!!!☆");
+    oWari.push_back("おめでとう👏");
 
-    std::cin >> a >> b >> x >> y;
-    std::cout << sumPair(a, b) << std::endl;
-    std::cout << std::fixed << std::setprecision(2) << sumPair(x, y) << std::endl;
-
+    for(auto &out: oWari){
+        if(out.find("👏") != std::string::npos){
+            for(char *a = out.data(); *a != '\0'; a++)
+                std::cout << out << " * " << int(a-out.data()+1) << std::endl;
+        }
+        std::cout << out << std::endl;
+    }
+    
     return 0;
 }
